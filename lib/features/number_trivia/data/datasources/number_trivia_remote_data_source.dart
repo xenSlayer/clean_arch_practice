cf88285261edba4a284255515960a12f04c23612
xenSlayer <kiranpaudel1892@gmail.com>
2020-07-28 11:54:58 +0545

    haha

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -37,7 +37,7 @@ class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
       return NumberTriviaModel.fromJson(json.decode(response.body));
     } else {
       throw (ServerException());
-      // print('hello there boss');
+      // print('hello there boss haha');
     }
   }
 }
